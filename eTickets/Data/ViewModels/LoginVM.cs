4120a21a479d24156361c8b8591e87bd8413897c
AdmOzk <adem.ozkyt@gmail.com>
2024-06-30 01:34:26 +0300

    loginview

---
+++ eTickets/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class LoginVM
+    {
+
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Password ")]
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
